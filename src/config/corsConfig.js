433eab2bd998258b5931f678c52a3bf777574aef
amnasmlp <amnas2229@gmail.com>
2024-07-19 13:22:25 +0530

    updated cors configuration

--- src/config/corsConfig.js
+++ src/config/corsConfig.js
@@ -3,7 +3,7 @@
 const cors = require('cors');
 
 const corsOptions = {
-  origin: ['http://localhost:3000'],
+  origin: ['http://localhost:3000','https://www.mylittlerivercard.eu','https://www.mylittlerivercard.com'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
